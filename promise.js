bcda2d391d7273d76897a07d04fef4e77757e902
swapnil <swapnil130501@gmail.com>
2024-12-30 14:01:06 +0530

    update custom promise

--- promise.js
+++ promise.js
@@ -7,86 +7,65 @@ var PromiseState;
 var MyPromise = /** @class */ (function () {
     function MyPromise(executor) {
         this._state = PromiseState.PENDING;
-        this._successCallbackHandlers = [];
-        this._failureCallbackHandlers = [];
-        this._finallyCallbackHandler = undefined;
+        this._onFulfillmentArray = [];
+        this._onRejectionArray = [];
         this._value = undefined;
-        this._reason = undefined;
         executor(this._promiseResolver.bind(this), this._promiseRejector.bind(this));
     }
-    MyPromise.prototype.then = function (handlerFn) {
-        var _this = this;
-        return new MyPromise(function (resolve, reject) {
-            var processHandler = function () {
-                try {
-                    var result = handlerFn(_this._value);
-                    if (result instanceof MyPromise) {
-                        result.then(resolve).catch(reject);
-                    }
-                    else {
-                        resolve(result);
-                    }
-                }
-                catch (error) {
-                    reject(error);
-                }
-            };
-            if (_this._state === PromiseState.FULFILLED) {
-                setTimeout(processHandler, 0);
-            }
-            else {
-                _this._successCallbackHandlers.push(function () { return setTimeout(processHandler, 0); });
-            }
-        });
-    };
-    MyPromise.prototype.catch = function (handlerFn) {
-        if (this._state === PromiseState.REJECTED) {
-            handlerFn(this._reason);
+    MyPromise.prototype.then = function (handlerCb) {
+        if (this._state === PromiseState.FULFILLED) {
+            handlerCb(this._value);
         }
         else {
-            this._failureCallbackHandlers.push(handlerFn);
+            this._onFulfillmentArray.push(handlerCb);
         }
         return this;
     };
-    MyPromise.prototype.finally = function (handlerFn) {
-        if (this._state !== PromiseState.PENDING)
-            return handlerFn();
-        this._finallyCallbackHandler = handlerFn;
+    MyPromise.prototype.catch = function (handlerCb) {
+        if (this._state === PromiseState.REJECTED) {
+            handlerCb(this._value);
+        }
+        else {
+            this._onRejectionArray.push(handlerCb);
+        }
+        return this;
+    };
+    MyPromise.prototype.finally = function (handlerCb) {
+        if (this._state !== PromiseState.PENDING) {
+            return handlerCb();
+        }
+        this._finallyCb = handlerCb;
     };
     MyPromise.prototype._promiseResolver = function (value) {
-        if (this._state === PromiseState.FULFILLED)
+        console.log("Inside promise resolver", this._state);
+        if (this._state === PromiseState.FULFILLED) {
             return;
+        }
         this._state = PromiseState.FULFILLED;
         this._value = value;
-        this._successCallbackHandlers.forEach(function (cb) { return cb(value); });
-        if (this._finallyCallbackHandler)
-            this._finallyCallbackHandler();
+        console.log("Running callbacks", this._onFulfillmentArray);
+        this._onFulfillmentArray.forEach(function (cb) { return cb(value); });
     };
-    MyPromise.prototype._promiseRejector = function (reason) {
-        if (this._state === PromiseState.REJECTED)
+    MyPromise.prototype._promiseRejector = function (value) {
+        if (this._state === PromiseState.REJECTED) {
             return;
+        }
         this._state = PromiseState.REJECTED;
-        this._reason = reason;
-        this._failureCallbackHandlers.forEach(function (cb) { return cb(reason); });
-        if (this._finallyCallbackHandler)
-            this._finallyCallbackHandler();
+        this._value = value;
+        this._onRejectionArray.forEach(function (cb) { return cb(value); });
     };
     return MyPromise;
 }());
-var p1 = new MyPromise(function (resolve, reject) {
-    setTimeout(function () { return resolve(42); }, 1000);
+var p1 = new MyPromise(function (res, rej) {
+    rej(10);
 });
-p1
-    .then(function (value) {
-    console.log('Resolved with:', value); // Output: Resolved with: 42
-    return value + 10;
-})
-    .then(function (value) {
-    console.log('Chained result:', value); // Output: Chained result: 52
-})
-    .catch(function (error) {
-    console.error('Caught error:', error);
-})
-    .finally(function () {
-    console.log('Finally block executed');
+p1.then(function (value) {
+    console.log("promise resolved", value);
+}).then(function (value) {
+    value = value + 10;
+    console.log("promise resolved x2", value);
+}).catch(function (value) {
+    console.log("promise rejected", value);
+}).finally(function () {
+    console.log("finally called");
 });
--- promise.ts
+++ promise.ts
@@ -1,108 +1,95 @@
-type TPromiseResolve<T> = (value: T) => void;
-type TPromiseReject<T> = (reason: T) => void;
-
-type TPromiseExecutor<T, K> = (
-  resolve: TPromiseResolve<T>,
-  reject: TPromiseReject<K>
-) => void;
-
-type TPromiseThenCallback<T> = (value: T | undefined) => void;
-type TPromiseCatchCallback<T> = (reason: T | undefined) => void;
-type TPromiseFinallyCallback = () => void;
-
 enum PromiseState {
     PENDING = 'pending',
     FULFILLED = 'fulfilled',
     REJECTED = 'rejected',
 }
 
-class MyPromise<T, K> {
+type TPromiseResolve = (value) => void;
+type TPromiseReject = (value) => void;
+
+type TPromiseExecutor = (resolve: TPromiseResolve, reject: TPromiseReject) => void;
+
+type TPromiseThenCb = (value) => void;
+type TPromiseCatchCb = (value) => void;
+type TPromiseFinallyCb = () => void;
+
+class MyPromise {
     private _state: PromiseState = PromiseState.PENDING;
+    private _onFulfillmentArray: TPromiseThenCb[] = [];
+    private _onRejectionArray: TPromiseCatchCb[] = [];
+    private _finallyCb: TPromiseFinallyCb;
+    private _value = undefined;
 
-    private _successCallbackHandlers: TPromiseThenCallback<T>[] = [];
-    private _failureCallbackHandlers: TPromiseCatchCallback<K>[] = [];
-    private _finallyCallbackHandler: TPromiseFinallyCallback | undefined = undefined;
-
-    private _value: T | undefined = undefined;
-    private _reason: K | undefined = undefined;
-
-    constructor(executor: TPromiseExecutor<T, K>) {
-        executor(
-            this._promiseResolver.bind(this),
-            this._promiseRejector.bind(this)
-        );
+    constructor(executor: TPromiseExecutor) {
+        executor(this._promiseResolver.bind(this), this._promiseRejector.bind(this));
     }
 
-    public then<V>(
-        handlerFn: (value: T | undefined) => V | MyPromise<V, K>
-    ): MyPromise<V, K> {
-        return new MyPromise<V, K>((resolve, reject) => {
-            const processHandler = () => {
-                const result = handlerFn(this._value);
-                if (result instanceof MyPromise) {
-                    result.then(resolve).catch(reject);
-                } else {
-                    resolve(result as V);
-                }
-            };
-    
-            if (this._state === PromiseState.FULFILLED) {
-                setTimeout(processHandler, 0);
-            } else {
-                this._successCallbackHandlers.push(() => setTimeout(processHandler, 0));
-            }
-        });
-    }
-    
+    public then(handlerCb: TPromiseThenCb) {
+        if(this._state === PromiseState.FULFILLED) {
+            handlerCb(this._value);
+        }
 
-    public catch(handlerFn: TPromiseCatchCallback<K>) {
-        if (this._state === PromiseState.REJECTED) {
-            handlerFn(this._reason);
-        } else {
-            this._failureCallbackHandlers.push(handlerFn);
+        else {
+            this._onFulfillmentArray.push(handlerCb);
         }
 
         return this;
     }
 
-    public finally(handlerFn: TPromiseFinallyCallback) {
-        if (this._state !== PromiseState.PENDING) return handlerFn();
-        this._finallyCallbackHandler = handlerFn;
+    public catch(handlerCb: TPromiseCatchCb) {
+        if(this._state === PromiseState.REJECTED) {
+            handlerCb(this._value);
+        }
+        
+        else {
+            this._onRejectionArray.push(handlerCb);
+        }
+
+        return this;
     }
 
-    private _promiseResolver(value: T) {
-        if (this._state === PromiseState.FULFILLED) return;
+    public finally(handlerCb: TPromiseFinallyCb) {
+        if(this._state !== PromiseState.PENDING) {
+            return handlerCb();
+        }
+
+        this._finallyCb = handlerCb;
+    }
+
+    private _promiseResolver(value) {
+        console.log("Inside promise resolver", this._state);
+        if(this._state === PromiseState.FULFILLED) {
+            return;
+        }
+
         this._state = PromiseState.FULFILLED;
         this._value = value;
-        this._successCallbackHandlers.forEach((cb) => cb(value));
-        if (this._finallyCallbackHandler) this._finallyCallbackHandler();
+        console.log("Running callbacks", this._onFulfillmentArray);
+        this._onFulfillmentArray.forEach((cb) => cb(value));
     }
 
-    private _promiseRejector(reason: K) {
-        if (this._state === PromiseState.REJECTED) return;
+    private _promiseRejector(value) {
+        if(this._state === PromiseState.REJECTED) {
+            return;
+        }
+
         this._state = PromiseState.REJECTED;
-        this._reason = reason;
-        this._failureCallbackHandlers.forEach((cb) => cb(reason));
-        if (this._finallyCallbackHandler) this._finallyCallbackHandler();
+        this._value = value;
+        this._onRejectionArray.forEach((cb) => cb(value));
     }
 }
 
-const p1 = new MyPromise<number, string>((resolve, reject) => {
-    setTimeout(() => resolve(42), 1000);
-  });
-  
-  p1
-    .then((value) => {
-      console.log('Resolved with:', value); // Output: Resolved with: 42
-      return value! + 10;
-    })
-    .then((value) => {
-      console.log('Chained result:', value); // Output: Chained result: 52
-    })
-    .catch((error) => {
-      console.error('Caught error:', error);
-    })
-    .finally(() => {
-      console.log('Finally block executed');
-    });
-  
+const p1 = new MyPromise((res, rej) => {
+    rej(10);
+})
+
+p1.then((value) => {
+    console.log("promise resolved", value);
+}).then((value) => {
+    value = value + 10;
+    console.log("promise resolved x2", value);
+}).catch((value) => {
+    console.log("promise rejected", value);
+}).finally(() => {
+    console.log("finally called")
+})
